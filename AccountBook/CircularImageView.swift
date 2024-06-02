37861b8e87649a9d464fe47e975624fd75f96539
Voltline <44248193@qq.com>
2024-06-02 23:47:47 +0800

    增加一系列图标，增加主界面的标题栏

---
+++ AccountBook/CircularImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularImageView.swift
+//  AccountBook
+//
+//  Created by 张艺怀 on 2024/6/2.
+//
+
+import SwiftUI
+
+struct CircularImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularImageView()
+}
---
+++ AccountBook/UserView.swift
@@ -0,0 +1,18 @@
+//
+//  UserView.swift
+//  AccountBook
+//
+//  Created by 张艺怀 on 2024/6/2.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserView()
+}
