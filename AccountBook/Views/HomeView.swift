f4c53ca23ed330fb8a1ca6f56fc634f08a8f22db
Voltline <44248193@qq.com>
2024-07-03 15:54:19 +0800

    [Feature]: 一些重大更新 * 增加底部栏，现在将程序分为主页、记录页和设置页三页 * 定时提醒的设置移动到了设置页 * 主页增加了已记录的所有账目中的分类别统计信息

---
+++ AccountBook/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  AccountBook
+//
+//  Created by 张艺怀 on 2024/7/3.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ AccountBook/Views/SettingBar.swift
@@ -0,0 +1,18 @@
+//
+//  SettingBar.swift
+//  AccountBook
+//
+//  Created by 张艺怀 on 2024/7/3.
+//
+
+import SwiftUI
+
+struct SettingBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingBar()
+}
