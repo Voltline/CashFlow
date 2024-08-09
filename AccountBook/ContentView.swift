574fecf3d03221af4540d0d1f73c49ac64de792e
Voltline <44248193@qq.com>
2024-08-09 17:41:20 +0800

    [Fix]: 更新版本号

--- AccountBook/ContentView.swift
+++ AccountBook/ContentView.swift
@@ -11,7 +11,7 @@ import AudioToolbox
 import LocalAuthentication
 import UserNotifications
 
-let version = "1.2.38.0809"
+let version = "1.2.39.0809"
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
